d4a326cc96ab4279df61dc2a573be1293692dde5
Guilherme <gui.bellissimo@gmail.com>
2025-03-12 11:32:17 -0300

    import correct components slices

--- src/pages/slice-simulator.tsx
+++ src/pages/slice-simulator.tsx
@@ -1,7 +1,8 @@
+'use client';
+
 import { SliceSimulator } from '@slicemachine/adapter-next/simulator';
 import { SliceZone } from '@prismicio/react';
-
-import { components } from '../slices';
+import { components } from '../../slices';
 
 export default function SliceSimulatorPage() {
   return (
